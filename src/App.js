75a630da01ddd32697a404ee4a47e814d5d541aa
kimtree24 <ktj503@naver.com>
2024-07-22 09:40:37 +0900

    FEAT: Home.jsx 파일 추가. 웹페이지 title 변경. 전역스타일 App.css 수정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home'; // 대소문자 에러 무시 - 얘 또 왜 이러는지 모르게씀...
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
