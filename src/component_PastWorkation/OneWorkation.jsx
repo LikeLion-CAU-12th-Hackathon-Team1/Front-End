220ae3b2b73bc9dd978bac2520c2c96381db75a3
2zooo0 <c20221460@cau.ac.kr>
2024-08-02 02:58:54 +0900

    feat: 지난워케이션 목록

---
+++ src/component_PastWorkation/OneWorkation.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+import Pin from "../assets/img/pin.svg"
+import day2 from "../assets/img/day2.svg"
+
+const OneWorkation = ({ workation_id, SiggText, formattedStartDate, formattedEndDate }) => {
+
+
+  return (
+    <One>
+        <ImgC src={day2}/>
+        <TextBox>{formattedStartDate} ~ {formattedEndDate}</TextBox>
+        <ImgL src={Pin}/>
+        <TextBox >{SiggText} 워케이션</TextBox>
+
+
+
+    </One>
+  )
+}
+
+export default OneWorkation
+
+const One = styled.div`
+    width: 93%;
+    height: 56px;
+    border: 1px solid #FEE0AA;
+    background-color: #FFFFFF;
+    border-radius: 8px;
+    padding: 3px 32px;
+    margin-bottom: 2%;
+    display: flex;
+    /* justify-content: center; */
+    align-items: center;
+`
+const ImgC = styled.img`
+    width: 32px;
+    height: 32px;
+`
+const ImgL = styled.img`
+    width: 32px;
+    height: 32px;
+`
+const TextBox = styled.div`
+    font-size: 20px;
+    font-weight: 600;
+    width: 41%;
+    letter-spacing: -0.02em; 
+    margin-left: 1%;
+`
